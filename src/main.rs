74d2f49da16898bd63cb2cba0d4d9d6380aa248a
김현석 <kim010322@gmail.com>
2024-06-01 10:36:51 +0000

    add sample BASIC code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let sample_code = String::from(
+        r#"
+        PRINT "How many fibonacci numbers do you want?"
+        INPUT nums
+        
+        LET a = 0
+        LET b = 1
+        WHILE nums > 0 REPEAT
+            PRINT a
+            LET c = a + b
+            LET a = b
+            LET b = c
+            LET nums = nums - 1
+        ENDWHILE
+    "#);
+
+    println!("List of tokens:");
+
+    let tokens = tokenize(sample_code);
+    println!("{tokens:?}");
+}
+
+pub fn tokenize(code: String) -> Vec<String> {
+    unimplemented!();
 }
